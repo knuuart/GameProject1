5732be6850ad98838e5b35c4617fe7bb04f00636
arttu <arttu.knuutinen@gmail.com>
2017-04-25 23:23:22 +0300

    Toivottavasti tästä ei nyt tuu liian iso ku tunkee kaikkea turhaa standard assettia

---
+++ TestChamber/Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour {
+    public GameObject playerCamera, portal, otherPortal;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // CopyPaste coding didn't work WutFace
+        //var portalPos = portal.transform.position;
+        //var otherPortalPos = otherPortal.transform.position;
+        //var playerCameraPos = playerCamera.transform.position;
+
+        //var playerOffsetFromPortal = playerCameraPos - otherPortalPos;
+        //transform.position = portalPos + playerOffsetFromPortal;
+
+        //var angleDifferenceBetweenPortalRotations = Quaternion.Angle(portal.transform.rotation, otherPortal.transform.rotation);
+        //var portalRotationalDifference = Quaternion.AngleAxis(angleDifferenceBetweenPortalRotations, Vector3.up);
+        //var newFacing = portalRotationalDifference * playerCamera.transform.forward;
+        //transform.rotation = Quaternion.LookRotation(newFacing, Vector3.up);
+	}
+}
---
+++ TestChamber/Assets/Scripts/CameraSpin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSpin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Camera.main.transform.Rotate(Vector3.up, 30f * Time.deltaTime);
+	}
+}
