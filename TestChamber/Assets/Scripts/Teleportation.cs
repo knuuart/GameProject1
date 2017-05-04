b3788ef0a16b2ae9a49e2061cadab8bbf1328134
arttu <arttu.knuutinen@gmail.com>
2017-05-04 23:24:31 +0300

    added rotation to teleport

--- TestChamber/Assets/Scripts/Teleportation.cs
+++ TestChamber/Assets/Scripts/Teleportation.cs
@@ -8,19 +8,23 @@ public class Teleportation : MonoBehaviour {
     //Figure out how to preserve momentum you dumdum
     //stopping continous back and forth teleporting should be in other gameobjects/scripts so cubes/player won't block eachother from porting
 
-    public GameObject otherPortal;
+    public GameObject otherPortal, portal;
     GameObject go;
     bool usedPortal = false;
     float time, allowPort = 0.2f;
     Vector3 velocity;
+    float portalAnglesY;
+    int rotationAngle;
 
     private void OnTriggerEnter(Collider other) {
         go = other.gameObject;
         //velocity = go.GetComponent<Rigidbody>().velocity;
         if (usedPortal == false && otherPortal.GetComponent<Teleportation>().usedPortal == false) {
+            go.transform.Rotate(0f, rotationAngle, 0f);
             go.transform.position = otherPortal.transform.position;
-            //go.GetComponent<Rigidbody>().velocity += velocity / 4; 
+            
             usedPortal = true;
+
         }
         
     }
@@ -30,7 +34,8 @@ public class Teleportation : MonoBehaviour {
     }
 
     void Start () {
-
+        portal.GetComponent<Transform>();
+        otherPortal.GetComponent<Transform>();
 	}
     private void OnTriggerExit(Collider other) {
         
@@ -38,6 +43,19 @@ public class Teleportation : MonoBehaviour {
 
     void Update () {
         ResetPortal();
+        portalAnglesY = portal.transform.rotation.y - otherPortal.transform.rotation.y;
+        if (portalAnglesY == 1f || portalAnglesY == -1f) {
+            rotationAngle = 0;
+        } else if (portalAnglesY < -0.1f) {
+            rotationAngle = -90;
+        } else if (portalAnglesY > 0.1f) {
+            rotationAngle = 90;
+        } else if (portalAnglesY == 0) {
+            rotationAngle = 180;
+        }
+        
+        
+        print(portalAnglesY);
 	}
     void ResetPortal() {
         if (usedPortal || otherPortal.GetComponent<Teleportation>().usedPortal) {
