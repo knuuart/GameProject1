9288cae1fc170426612eaf115259948528643ea5
Leo Nikander <leo.nigander@gmail.com>
2017-05-08 20:16:18 +0300

    hoidelin lisää juttui mun scenessä.

--- TestChamber/Assets/Scripts/leotest/playerBehaviour.cs
+++ TestChamber/Assets/Scripts/leotest/playerBehaviour.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class playerBehaviour : MonoBehaviour {
 
     public float movePower;
+    public float colSphereRadius;
+    public LayerMask sphereCastLayer;
     float movX;
     float movY;
     float lookX;
     float lookY;
     public Rigidbody rb;
+    public Vector3 spherePosition;
     Vector3 inputDirection;
     public Camera cam; 
 
@@ -26,7 +29,23 @@ public class playerBehaviour : MonoBehaviour {
 
     private void FixedUpdate() {
 
+        //AAA
+
+        rb.velocity = rb.velocity + inputDirection.normalized * movePower * Time.fixedDeltaTime;
+
+        Ray sphereCast;
+        RaycastHit hitinfo;
+
+        Physics.SphereCast(spherePosition, colSphereRadius, Vector3.zero, out hitinfo, Mathf.Infinity, sphereCastLayer);
+
+        
+    }
+    
+    void OnCollisionEnter(Collision coll) {
+        rb.drag = 5;
     }
 
-    
+    void OnCollisionExit(Collision coll) {
+        rb.drag = 0;
+    }
 }
