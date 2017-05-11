df0a539ca8da278d0d7236f7058d252c23a2cc9e
arttu <arttu.knuutinen@gmail.com>
2017-05-12 00:30:05 +0300

    joku huono teleporttitsydeemi

--- TestChamber/Assets/Scripts/ShootPortal.cs
+++ TestChamber/Assets/Scripts/ShootPortal.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class ShootPortal : MonoBehaviour {
 
     public GameObject orangePortal, bluePortal;
+    public Quaternion orangeRotation, blueRotation;
+    public Collider behindBlue, behindOrange;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -15,14 +17,13 @@ public class ShootPortal : MonoBehaviour {
 	void Update () {
         if (Input.GetMouseButtonDown(0)) {
             CreatePortal(bluePortal);
-
         }
         if (Input.GetMouseButtonDown(1)) {
             CreatePortal(orangePortal);
         }
     }
 
-    void CreatePortal(GameObject portal) {
+    public void CreatePortal(GameObject portal) {
 		portal.SetActive(true);
         int x = Screen.width / 2;
         int y = Screen.height / 2;
@@ -30,9 +31,18 @@ public class ShootPortal : MonoBehaviour {
         Ray ray = Camera.main.ScreenPointToRay(new Vector3(x, y));
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit)) {
-            Quaternion hitObjectRotation = Quaternion.LookRotation(hit.normal * -1);
+            Quaternion hitObjectRotation = Quaternion.LookRotation(hit.normal);
             portal.transform.position = hit.point;
             portal.transform.rotation = hitObjectRotation;
+            //if (portal == bluePortal) {
+            //    blueRotation = hitObjectRotation;
+            //    behindBlue = hit.collider;
+
+            //}
+            //if (portal == orangePortal) {
+            //    orangeRotation = hitObjectRotation;
+            //    behindOrange = hit.collider;
+            //}
         }
     }
 }
--- TestChamber/Assets/Scripts/TeleportationV2.cs
+++ TestChamber/Assets/Scripts/TeleportationV2.cs
@@ -4,33 +4,63 @@ using UnityEngine;
 
 public class TeleportationV2 : MonoBehaviour {
 
-	public Transform bluePortal, orangePortal;	
+	public GameObject bluePortal, orangePortal;
+    Quaternion blueNormal, orangeNormal;
+    ShootPortal sp;
+    bool hasPorted;
 
     private void OnTriggerEnter(Collider other) {
-        	
-//		if (other.tag == "BluePortal") {
-//			transform.position = orangePortal.transform.position;
-//		}
-//		if (other.tag == "OrangePortal") {
-//			transform.position = bluePortal.transform.position;
-//		}
-		Physics.IgnoreLayerCollision(9, 8);
+        if (other.tag == "BluePortal" && hasPorted == false) {
+            transform.position = orangePortal.transform.position;
+            hasPorted = true;
+            StartCoroutine("Reset");
+
+        }
+        if (other.tag == "OrangePortal" && hasPorted == false) {
+            transform.position = bluePortal.transform.position;
+            hasPorted = true;
+            StartCoroutine("Reset");
+        }
+        //if(other.tag == "BlueMesh") {
+        //    Physics.IgnoreCollision(gameObject.GetComponent<Collider>(), sp.behindBlue);
+        //}
+        //if (other.tag == "OrangeMesh") {
+        //    Physics.IgnoreCollision(gameObject.GetComponent<Collider>(), sp.behindOrange);
+        //}
+
+
+        Physics.IgnoreLayerCollision(9, 8);
     }
 
     private void OnTriggerStay(Collider other) {
-
     }
 
     void Start () {
         
 	}
     private void OnTriggerExit(Collider other) {
+        //if (other.tag == "BlueMesh") {
+        //    Physics.IgnoreCollision(gameObject.GetComponent<Collider>(), sp.behindBlue, false);
+        //}
+        //if (other.tag == "OrangeMesh") {
+        //    Physics.IgnoreCollision(gameObject.GetComponent<Collider>(), sp.behindOrange, false);
+        //}
 
-		Physics.IgnoreLayerCollision(8, 9, false);
-        
+        Physics.IgnoreLayerCollision(8, 9, false);
+
     }
 
     void Update () {
-		
-	}
+        //if(hasPorted == true) {
+        //    StartCoroutine("Reset");
+        //}
+        //if (Input.GetKeyDown(KeyCode.F)) {
+        //    Debug.DrawRay(gameObject.transform.position, sp.behindBlue.transform.position);
+
+        //}
+    }
+    IEnumerator Reset() {
+        yield return new WaitForSeconds(0.5f);
+        hasPorted = false;
+    }
 }
