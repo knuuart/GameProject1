c9e29d8d822fc503372e3620453a700263c6786c
Leo Nikander <leo.nikander@laajasalonopisto.fi>
2017-05-29 13:59:31 +0300

    Nappulan ja oven kanssa riehumista, kumpikaan ei ole vielä valmis.

--- TestChamber/Assets/Scripts/LevelEntitites/BigButton/BigButtonBehaviour.cs
+++ TestChamber/Assets/Scripts/LevelEntitites/BigButton/BigButtonBehaviour.cs
@@ -6,23 +6,41 @@ using UnityEngine;
 public class BigButtonBehaviour : MonoBehaviour {
 
     [Header("Button Properties")]
+    [Space(10)]
     [Tooltip("Minimum mass of the other rigidbody required for actiavtion")]
     public float activationMass;
 
     [Space(10)]
     [Tooltip("When the button is pushed down")]
     public UnityEvent Activate;
+    [Space(10)]
     [Tooltip("When the button gets back up")]
     public UnityEvent DeActivate;
 
+
+    private float weightOnButton;
+    private List<GameObject> objectsOnButton = new List<GameObject>();
+
     void OnCollisionEnter(Collision coll) {
-        if (coll.rigidbody.mass >= activationMass) {
-            Activate.Invoke();
+        if (coll.gameObject.GetComponent<Rigidbody>() != null) {
+            var rb = coll.gameObject.GetComponent<Rigidbody>();
+            weightOnButton += rb.mass;
+            ButtonStateCheck();
         }
     }
 
     private void OnCollisionExit(Collision coll) {
-        if (coll.rigidbody.mass >= activationMass) {
+        if (coll.gameObject.GetComponent<Rigidbody>() != null) {
+            var rb = coll.gameObject.GetComponent<Rigidbody>();
+            weightOnButton -= rb.mass;
+            ButtonStateCheck();
+        }
+    }
+
+    private void ButtonStateCheck() {
+        if (weightOnButton >= activationMass) {
+            Activate.Invoke();
+        } else {
             DeActivate.Invoke();
         }
     }
---
+++ TestChamber/Assets/Scripts/animationBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationBehaviour : MonoBehaviour {
+
+    public Animation anim;
+
+    public void AnimationPlay() {
+        anim.Play();
+    }
+
+    public void AnimationRewind() {
+        anim.Rewind("doorOpen");
+    }
+}
