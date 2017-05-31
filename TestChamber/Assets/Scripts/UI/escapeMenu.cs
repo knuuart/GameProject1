625ffece1fb7b675fdc94468870c902d3913ce9c
Leo Nikander <leo.nikander@laajasalonopisto.fi>
2017-05-31 11:09:20 +0300

    Lisätty Escape Menu Handler. Tiputa skeneen niin siellä on escape menu kun painat esc

---
+++ TestChamber/Assets/Scripts/UI/escapeMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class escapeMenu : MonoBehaviour {
+
+    public GameObject panel;
+    public bool windowFocused;
+    [SerializeField] private bool menuVisible;
+
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update() {
+        if (Input.GetButtonDown("Cancel")) {
+            menuVisible = !menuVisible;
+        }
+
+        if (menuVisible) {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+        } else {
+            Cursor.visible = false;
+        }
+
+        panel.active = menuVisible;
+
+        //What happens when the game window is focused
+        if (windowFocused && !menuVisible) {
+            Cursor.lockState = CursorLockMode.Locked;
+        } else if (windowFocused && menuVisible) {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+
+    public void QuitButton() {
+        Application.Quit();
+        Debug.Log("Game is trying to quit");
+    }
+
+    private void OnApplicationFocus(bool focus) {
+        windowFocused = focus;
+    }
+}
