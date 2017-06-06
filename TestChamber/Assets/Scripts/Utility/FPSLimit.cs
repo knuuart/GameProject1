93dd9272115fa2d2096511331cf29310793c7471
knuuart <arttu.knuutinen@gmail.com>
2017-06-06 10:43:27 +0300

    valoprefab, pikkusäätöä

--- TestChamber/Assets/Scripts/Utility/FPSLimit.cs
+++ TestChamber/Assets/Scripts/Utility/FPSLimit.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FPSLimit : MonoBehaviour {
     public int fpsLimit = 60;
 
     // Use this for initialization
-    void Awake() {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = fpsLimit;
-    }
+//    void Awake() {
+//        QualitySettings.vSyncCount = 0;
+//        Application.targetFrameRate = fpsLimit;
+//    }
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.L)) {
+			SceneManager.LoadScene ("Portaltest");
+		}
+	}
 }
